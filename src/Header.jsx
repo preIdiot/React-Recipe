f857849bd74a3cc72b053ac2063e5e04d195aaa2
shayash22 <shayash2203@gmail.com>
2021-08-04 19:24:25 +0530

    gjhgjg

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-function Header(){
-    return(
-        <div>
-       {/* navbar start */}
+function Header() {
+  return (
+    <div>
+      {/* navbar start */}
       <div className="bg-img">
-        <nav className="navbar navBg navbar-expand-lg" style="height: 120px">
+        <nav className="navbar navBg navbar-expand-lg">
           <div className="container-fluid">
-          
-            <a href="index.html"><img className="img-responsive2 navLogo" src="images/Logo1.png"/></a>
+            <a href="index.html">
+              <img
+                className="img-responsive2 navLogo"
+                src="images/Logo1.png"
+                alt="Logo"
+              />
+            </a>
 
             <button
               className="navbar-toggler"
@@ -26,41 +30,40 @@ function Header(){
             <div
               claclassNames="collapse navbar-collapse w-150 flex-sm-column"
               id="navbarCollapse"
-              style="margin-left: 45%"
             >
               <ul>
                 <li>
-                  <span className="social-number"
-                    ><i className="fa fa-phone" aria-hidden="true">
-                      : +91 94394 45511</i
-                    ></span
-                  >
+                  <span className="social-number">
+                    <i className="fa fa-phone" aria-hidden="true">
+                      : +91 94394 45511
+                    </i>
+                  </span>
                 </li>
 
                 <li>
                   <span className="social-icon social-facebook">
-                    <i className="fa fa-facebook"></i
-                  ></span>
+                    <i className="fa fa-facebook"></i>
+                  </span>
                 </li>
                 <li>
-                  <span className="social-icon social-google"
-                    ><i className="fa fa-google"></i
-                  ></span>
+                  <span className="social-icon social-google">
+                    <i className="fa fa-google"></i>
+                  </span>
                 </li>
                 <li>
-                  <span className="social-icon social-linkedin"
-                    ><i className="fa fa-linkedin"></i
-                  ></span>
+                  <span className="social-icon social-linkedin">
+                    <i className="fa fa-linkedin"></i>
+                  </span>
                 </li>
                 <li>
-                  <span className="social-icon social-instagram"
-                    ><i className="fa fa-instagram"></i
-                  ></span>
+                  <span className="social-icon social-instagram">
+                    <i className="fa fa-instagram"></i>
+                  </span>
                 </li>
                 <li>
-                  <span className="social-icon social-twitter"
-                    ><i className="fa fa-twitter"></i
-                  ></span>
+                  <span className="social-icon social-twitter">
+                    <i className="fa fa-twitter"></i>
+                  </span>
                 </li>
               </ul>
 
@@ -69,12 +72,10 @@ function Header(){
                 <div className="input-group">
                   <div id="google_translate_element"></div>
                   &nbsp&nbsp
-
                   <input
                     type="text"
                     className="form-control border-dark btn-sm posRecipe"
                     placeholder="RECIPE"
-                    style="border-radius: 1rem 0rem 0rem 1rem"
                   />
                   <div className="input-group-append">
                     <button
@@ -95,17 +96,19 @@ function Header(){
       <div className="navbar2 navbar-expand-lg bgColNav">
         <div className="container-md">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item nav-Link" href="#" style="margin-left: -55px">
+            <li
+              className="nav-item nav-Link"
+              href="#"
+            
+            >
               RECIPE
             </li>
           </ul>
         </div>
       </div>
-{/* <!-- navbar ended --> */}
-</div>
-
-    ); 
-
+      {/* <!-- navbar ended --> */}
+    </div>
+  );
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
+import "./index.css";
 
 ReactDOM.render(
   <Header/>,
