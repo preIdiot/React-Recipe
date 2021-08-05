a65bf5c55c4a5f77cac6216e06c10e51525e0b22
jatinjain001 <jatinjain20022001@gmail.com>
2021-08-05 16:51:38 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from "./Header";
 import Scroll from "./Components/Scroll.jsx";
 import "./index.css";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="scroll">
       <Scroll/>
       </div>
+      <Footer/>
     </div>
   );
 }
