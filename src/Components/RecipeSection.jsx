9ce251ab4a9ce55963964cf5d6309454aa8b70d3
shayash22 <shayash2203@gmail.com>
2021-08-08 21:51:28 +0530

    update

--- src/Components/RecipeSection.jsx
+++ src/Components/RecipeSection.jsx
@@ -8,49 +8,49 @@ function RecipeSection(props) {
     return (
         <div>
 
-            <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-8">
             <center><h1>{props.title}</h1></center>
             </div>
-            <div class="col-md-2">
-                <button type="button" class="btn btn-light btn-lg" onclick="window.print()"><i class="fa fa-print" aria-hidden="true"></i>Print</button>
+            <div className="col-md-2">
+                <button type="button" className="btn btn-light btn-lg" onclick="window.print()"><i className="fa fa-print" aria-hidden="true"></i>Print</button>
             </div>
             </div>
             <br/>
-            <div class="row">
-                <div class="col-md-1"></div>
-                    <div class="col-md-3">
-                        <div class="container-fluid">
-                            <img class="img-responsive2 navLogo imgStyle" src={props.imgsrc}/>
+            <div className="row">
+                <div className="col-md-1"></div>
+                    <div className="col-md-3">
+                        <div className="container-fluid">
+                            <img className="img-responsive2 navLogo imgStyle" src={props.imgsrc}/>
                         </div>
                     </div>
-                    <div class="col-md-1"></div>
+                    <div className="col-md-1"></div>
 
 
-                    <div class="col-md-6">
+                    <div className="col-md-6">
                         <p>
                             {props.desc}
                         </p>
-                        <div class="card-body">
+                        <div className="card-body">
            
                         <div>
-                            <i i class="fa fa-cutlery fa-lg" style="margin-right: 5rem;"> Recipe Servings:{props.recipeServings}</i>
-                            <i i class="fa fa-clock-o fa-lg" style="margin-top: 6px;">Pre Time:{props.preTime}</i>
+                            <i  className="fa fa-cutlery fa-lg" style="margin-right: 5rem;"> Recipe Servings:{props.recipeServings}</i>
+                            <i  className="fa fa-clock-o fa-lg" style="margin-top: 6px;">Pre Time:{props.preTime}</i>
                         </div>
                         <br/>
                         <div>
-                            <i class="fa fa-clock-o fa-lg" style="margin-right: 5rem;"> Cook Time:{props.cookTime}</i>
-                            <i class="fa fa-clock-o fa-lg" style="margin-top: 6px;">Total Time:{props.totalTime}</i>
+                            <i className="fa fa-clock-o fa-lg" style="margin-right: 5rem;"> Cook Time:{props.cookTime}</i>
+                            <i className="fa fa-clock-o fa-lg" style="margin-top: 6px;">Total Time:{props.totalTime}</i>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-1"></div>
+                <div className="col-md-1"></div>
             </div>
 
             <br/><br/>
-           <div class="row"> 
-              <div class="col-md-4 part1">         
+           <div className="row"> 
+              <div className="col-md-4 part1">         
                 <h4>
                   Ingredients Of {props.title}</h4>
                   <br/>
@@ -76,9 +76,9 @@ function RecipeSection(props) {
                 </h5>
               </div>
 
-              <div class="col-md-1"></div>
+              <div className="col-md-1"></div>
 
-              <div class="col-md-6 part2">
+              <div className="col-md-6 part2">
                 <h3>How to Make {props.title}</h3>
                 <br/>
                 <p>{props.st1}</p>
