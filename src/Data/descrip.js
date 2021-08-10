52fdc36fbdb3240214d369159d8791465b422c14
Srishti2203 <52817767+Srishti2203@users.noreply.github.com>
2021-08-10 19:48:12 +0530

    change

--- src/Data/descrip.js
+++ src/Data/descrip.js
@@ -15,16 +15,235 @@ export  const Data=[
         ]
       
       },
+
+      {
+        "subid":1 ,
+        "recipe1":"sandwich",
+        "description":"abhdinsifa",
+        "steps":[
+          "take some breads",
+          "some amount of butter",
+          ]
+        
+      },
+
+      {
+        "subid":1 ,
+        "recipe1":"sandwich",
+        "description":"abhdinsifa",
+        "steps":[
+          "take some breads",
+          "some amount of butter",
+          ]
+        
+        },
+  
+        {
+          "subid":1 ,
+          "recipe1":"sandwich",
+          "description":"abhdinsifa",
+          "steps":[
+            "take some breads",
+            "some amount of butter",
+            ]
+          
+        },
+
+        {
+          "subid":1 ,
+          "recipe1":"sandwich",
+          "description":"abhdinsifa",
+          "steps":[
+            "take some breads",
+            "some amount of butter",
+            ]
+          
+        },
+    
+          {
+            "subid":1 ,
+            "recipe1":"sandwich",
+            "description":"abhdinsifa",
+            "steps":[
+              "take some breads",
+              "some amount of butter",
+              ]
+            
+          },
+    
+          {
+            "subid":1 ,
+            "recipe1":"sandwich",
+            "description":"abhdinsifa",
+            "steps":[
+              "take some breads",
+              "some amount of butter",
+              ]
+            
+          },
+      
+          {
+              "subid":1 ,
+              "recipe1":"sandwich",
+              "description":"abhdinsifa",
+              "steps":[
+                "take some breads",
+                "some amount of butter",
+                ]
+              
+          },
+          {
+            "subid":1 ,
+            "recipe1":"sandwich",
+            "description":"abhdinsifa",
+            "steps":[
+              "take some breads",
+              "some amount of butter",
+              ]
+            
+            },
+      
+            {
+              "subid":1 ,
+              "recipe1":"sandwich",
+              "description":"abhdinsifa",
+              "steps":[
+                "take some breads",
+                "some amount of butter",
+                ]
+              
+            },
+      
+            {
+              "subid":1 ,
+              "recipe1":"sandwich",
+              "description":"abhdinsifa",
+              "steps":[
+                "take some breads",
+                "some amount of butter",
+                ]
+              
+              },
+              {
+                "subid":1 ,
+                "recipe1":"sandwich",
+                "description":"abhdinsifa",
+                "steps":[
+                  "take some breads",
+                  "some amount of butter",
+                  ]
+                
+              },
+          
+              {
+                  "subid":1 ,
+                  "recipe1":"sandwich",
+                  "description":"abhdinsifa",
+                  "steps":[
+                    "take some breads",
+                    "some amount of butter",
+                    ]
+                  
+              },
+              {
+                "subid":1 ,
+                "recipe1":"sandwich",
+                "description":"abhdinsifa",
+                "steps":[
+                  "take some breads",
+                  "some amount of butter",
+                  ]
+                
+                },
+          
+                {
+                  "subid":1 ,
+                  "recipe1":"sandwich",
+                  "description":"abhdinsifa",
+                  "steps":[
+                    "take some breads",
+                    "some amount of butter",
+                    ]
+                  
+                },
+          
+                {
+                  "subid":1 ,
+                  "recipe1":"sandwich",
+                  "description":"abhdinsifa",
+                  "steps":[
+                    "take some breads",
+                    "some amount of butter",
+                    ]
+                  
+                  },
+        
+              {
+                "subid":1 ,
+                "recipe1":"sandwich",
+                "description":"abhdinsifa",
+                "steps":[
+                  "take some breads",
+                  "some amount of butter",
+                  ]
+                
+              },
+      
+              {
+                "subid":1 ,
+                "recipe1":"sandwich",
+                "description":"abhdinsifa",
+                "steps":[
+                  "take some breads",
+                  "some amount of butter",
+                  ]
+                
+              },
+          
+                {
+                  "subid":1 ,
+                  "recipe1":"sandwich",
+                  "description":"abhdinsifa",
+                  "steps":[
+                    "take some breads",
+                    "some amount of butter",
+                    ]
+                  
+                },
+          
+                {
+                  "subid":1 ,
+                  "recipe1":"sandwich",
+                  "description":"abhdinsifa",
+                  "steps":[
+                    "take some breads",
+                    "some amount of butter",
+                    ]
+                  
+                },
+            
+                {
+                    "subid":1 ,
+                    "recipe1":"sandwich",
+                    "description":"abhdinsifa",
+                    "steps":[
+                      "take some breads",
+                      "some amount of butter",
+                      ]
+                    
+                },
+        
+
       ]
   },
   {
-    "id":1,
+    "id":2,
     "imgsrc":'../images/snackrecipies/Chicken Sub Sandwich.jpg',
     "name":"Snack recipe",
     "info":"Snack Recipies is those recipies which tooks as a light wieght food and it is ready in less time",
     "recipies":[
       {
-      "subid":1 ,
+      "subid":2 ,
       "recipe1":"sandwich",
       "description":"abhdinsifa",
       "steps":[
