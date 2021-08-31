6e4b35f425b111a105d9f94e4b26a6b8f398c0a8
jatinjain001 <jatinjain20022001@gmail.com>
2021-08-31 23:02:48 +0530

    video added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import "./index.css";
+import Video from "./Components/Video";
 import "../src/css/style.css"
 import Main from './main';
 
@@ -10,7 +11,10 @@ function App() {
   return (
     <div>
       <Header/>
+      <div className="row">
        <Main/>
+       <Video/>
+       </div>
       <Footer/>
 
     </div>
