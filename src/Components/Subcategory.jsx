4da893e58baef1ed22fed731f357fec3dd532227
Parag Katoch <paragkatoch5@gmail.com>
2021-09-23 22:09:46 +0530

    updated components

--- src/Components/Subcategory.jsx
+++ src/Components/Subcategory.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import Subcat from "./Subcat";
+import { useParams } from "react-router-dom";
+import { Data } from "../Data/descrip";
+import Search from "./Search";
 
 export default function SubCategory(props) {
+  let { recipename } = useParams();
+
+  const data =
+    props.data !== undefined
+      ? [props.data]
+      : Data.filter((recipes) => `/recipes/${recipename}` === recipes.link);
+
+  if (props.search !== "" && props.search !== undefined)
+    return <Search search={props.search} Data={data} />;
+
+  return <SubCategoryData {...{ data }} />;
+}
+
+function SubCategoryData({ data }) {
   return (
     <div className="scroll">
-      {props.data.recipies.map((val) => {
-        return (
-          <div>
-            <Subcat
-              key={val.subid}
-              // recipename={val.recipename}
-              // imgfile={val.imgfile}
-              // link={val.link}
-              // recipieServe={val.recipieServings}
-              // preTime={val.preTime}
-              // totalTime={val.totalTime}
-              // cookTime={val.cookTime}
-              {...{ info: props.data.info, ...val }}
-              info={props.data.info}
-            />
-          </div>
-        );
-      })}
+      {data[0].recipies.map((val) => (
+        <div>
+          <Subcat
+            key={val.subid}
+            {...{ info: data[0].info, ...val }}
+            info={data[0].info}
+          />
+        </div>
+      ))}
     </div>
   );
 }
